06a95210bbd2b7339c5881f7ddbdae3c2e8c4bbc
覃川川 <1037425668@qq.com>
2019-03-16 09:39:50 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">狗牙蔷薇</router-link> |
+      <router-link to="/about">个人简历</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 #nav {
   padding: 30px;
+  background-color: rgb(2,202,212);
 }
 
 #nav a {
@@ -26,6 +32,7 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  /*color: #42b983;*/
+  color: #fff;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,266 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+  	<div class="main">
+  		<div class="title">
+  			<h1 class="til">
+  				个人简历 
+  			</h1>
+  			<h3 class="job">
+  				求职意向：web前端工程师 
+  				<span class="job1">
+  					Job Search Intention: Web Front-end Engineer
+  				</span>
+  			</h3>
+  			<div class="box">
+  				
+  			</div>
+  		</div>
+  		<div class="msg">
+  			<div class="tip">
+  				<h3 class="tiptitle">
+  					基本信息
+  				</h3>	
+  			</div>
+  			<div class="content">
+  				<img src="../assets/chuan.jpg" width="180" height="220" class="portrait">
+  				<el-row :gutter="20">
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>姓    名	：覃川川</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>民    族	：壮	</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>电    话	：18778271265</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>邮    箱：1037425668@qq.com</span></div></el-col>
+				   <el-col :span="6"><div class="grid-content bg-purple"><span>住    址：广西柳州城中区268号</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>出生年月：1996.09</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>身    高：173cm</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>政治面貌：共青团员</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>毕业院校	：广西科技大学</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>学    历	：本科</span></div></el-col>
+				  <el-col :span="6"><div class="grid-content bg-purple"><span>专业	：软件工程</span></div></el-col>
+				</el-row>
+ 
+  			</div>
+  			<hr>
+  			<div class="tip">
+  				<h3 class="tiptitle">
+  					教育背景
+  				</h3>	
+  			</div>
+  			<div class="content1">
+  				<div class="school">
+  					<span>2015.09-2019.06</span>	
+					<span>广西科技大学</span>
+					<span>软件工程（本科）</span> 
+  				</div>
+  				<br>
+  				<div class="cour">
+  					<p>
+  						
+  							<strong>主修课程：</strong>	
+							C语言程序设计、C++面向对象编程、JAVA程序设计、数据结构与算法设计、<br><br>oracle大型数据库、操作系统、计算机网络，软件需求分析、软件工程经济学、UML建模等
+					
+  					</p>
+  				</div>                       
+			
+  			</div>
+  			<hr>
+  			<div class="tip">
+  				<h3 class="tiptitle">
+  					校园经历
+  				</h3>	
+  			</div>
+  			<div class="content1">
+  				<div class="experience">
+  					<p>
+  						2015.10——2017.06  &nbsp;&nbsp;&nbsp; 加入学院学生会，大二学生会干部
+  					</p><br>
+  					<p>
+  						2015.10——2017.06 &nbsp;&nbsp;&nbsp;  加入校风校纪纠察队，获"优秀学生干部"
+  					</p><br>
+  					<p>
+  						2017.03——2018.01  &nbsp;&nbsp;&nbsp; 加入校企联合实验室，学习web前端知识的学习
+  					</p><br>
+  					<p>
+  						2018.03——2018.06  &nbsp;&nbsp;&nbsp; 接受web前端设计的培训，学习Html,css,js和vue框架
+  					</p><br>
+  				</div>
+  				                      
+			
+  			</div>
+  			<hr>
+  			<div class="tip">
+  				<h3 class="tiptitle">
+  					实习经历
+  				</h3>	
+  			</div>
+  			<div class="content1">
+  				<div class="experience">
+  					2018.07——至今 <br>
+  					<p><br>
+  						 广西犇云科技股份有限公司 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工作岗位：前端工程师（实习生）<br><br>
+  						 负责项目前端客户端页面的开发、功能的实现和数据的渲染
+  					</p><br>
+  					
+  				</div>
+  				                      
+			
+  			</div>
+  			<hr>
+  			<div class="tip">
+  				<h3 class="tiptitle">
+  					相关技能
+  				</h3>	
+  			</div>
+  			<div class="content1">
+  				<div class="experience">
+  					
+  					<p><br>
+  						正在学习并使用vue3.0+element进行开发<br><br>
+  						熟悉页面布局和网页制作的方法&nbsp;html&nbsp;+&nbsp;css&nbsp;<br><br>
+  						熟悉前端开发框架&nbsp;bootstrap&nbsp;和&nbsp;layui&nbsp;的使用<br><br>
+  						熟悉js、jq和ajax的使用，对用户体验、交互操作流程有一定了解<br><br>
+  					</p><br>
+  					
+  				</div>
+  				                      
+			
+  			</div>
+  			<hr>
+  			<div class="tip">
+  				<h3 class="tiptitle">
+  					个人评价
+  				</h3>	
+  			</div>
+  			<div class="content1">
+  				<div class="experience">
+  					
+  					<p class="personMsg"><br>
+  						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;本人性格开朗，善于交际，有较强的责任心和团队合作精神，能与同事，其它部<br><br>门积极配合完成工作，能与上司保持良好的沟通，尊重上司的安排。工作认真负责，<br><br>不推卸责任，好学上进，希望在工作中不断提高工作能力，能承受工作中的压力，能<br><br>熬夜。我的兴趣爱好是游泳和打球，平时的锻炼有跑步和打篮球的锻炼，以保证平时<br><br>的锻炼量，这样能够拥有一个健康的身体去应付生活和工作。
+  					</p><br>
+  					
+  				</div>
+  				                      
+			
+  			</div>
+  		</div>
+
+  	</div>
+    <!-- <h1>一二三四五</h1> -->
   </div>
 </template>
+
+<style scoped="">
+
+	.personMsg{
+		width: 600px;
+		margin: 0 auto;
+	}
+	.experience{
+		padding: 30px;
+		margin: 0 auto;
+	}
+	/*.experience span{
+
+	}*/
+	.main{
+		width: 1200px;
+		/*border: solid 1px;*/
+		margin:  0 auto;
+		margin-top: 30px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+	}
+	.til{
+		padding: 5px;
+		font-size: 4em;
+		/*display:inline; */
+		text-align: left;
+		margin-left: 30px;
+		color: rgb(78,114,130);
+		width: 300px;
+		
+	}
+	.job{
+		/*float: left;*/
+		position: relative;
+		top: -40px;
+		left: 400px;
+		text-align: left;
+		/*margin-left: 50px;*/
+	}
+	.job1{
+		margin-left: 50px;
+	}
+	.box{
+		width: 1200px;
+		height: 30px;
+		background-color: rgb(78,114,130);
+	}
+	.tip{
+		width: 200px;
+		height: 40px;
+		background-color: rgb(78,114,130);
+		margin-top: 20px;
+	}
+	.msg{
+		width: 1100px;
+		margin: 0 auto;
+		 box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+	}
+	.content{
+		/*padding: 10px;*/
+		padding-top: 20px;
+	}
+	.content span{
+		margin-left: 10px;
+		float: left;
+		padding: 13px;
+	}
+	.portrait{
+		float: right;
+		margin-right: 50px;
+		/*top: -30px;*/
+		padding-bottom: 30px;
+	}
+	.tiptitle{
+		 line-height: 40px;
+		 color: #fff;
+	}
+	.school{
+		padding-top: 20px;
+		/*font-size: 1.1em;*/
+	}
+	.content1 span{
+		margin-left: 10px;
+		padding: 13px;
+	}
+	.cour{
+		padding: 20px;
+		font-size: 1.1em;
+	}
+
+
+	  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    /*background: #99a9bf;*/
+  }
+  .bg-purple {
+    /*background: #d3dce6;*/
+  }
+  .bg-purple-light {
+    /*background: #e5e9f2;*/
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
